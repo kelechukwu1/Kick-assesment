303fab5f6fa4c548b90a4e5197d2d5d65cca080d
Kelechukwu <paparazzip8@gmail.com>
2025-02-27 14:05:02 +0100

    update: removed unused codes

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -3,7 +3,6 @@ import SidebarTriggerButton from "@/components/partials/SidebarTriggerButton"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { Outlet } from "react-router-dom"
 
-
 const DashboardLayout = () => {
     return (
         <SidebarProvider>
@@ -17,22 +16,3 @@ const DashboardLayout = () => {
 }
 
 export default DashboardLayout
-
-// import { AppSidebar } from "@/components/partials/AppSidebar"
-// import SidebarTriggerButton from "@/components/partials/SidebarTriggerButton"
-// import {
-//     SidebarInset,
-//     SidebarProvider,
-// } from "@/components/ui/sidebar"
-
-// export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-//     return (
-//         <SidebarProvider className="flex overflow-hidden">
-//             <AppSidebar />
-//             {/* <SidebarTriggerButton /> */}
-//             <SidebarInset>
-//                 {children}
-//             </SidebarInset>
-//         </SidebarProvider>
-//     )
-// }
